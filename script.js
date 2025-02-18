b58c26406f8cfd3379e061a367017fafde4dff3d
ArielCoimbra <arielcoimbra2018@gmail.com>
2025-02-18 13:09:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function addProfile() {
-  alert("Função de adicionar perfil ainda não implementada!");
+function selectProfile(profileElement) {
+  // Remover a classe "selected" de todos os perfis
+  const profiles = document.querySelectorAll('.profile');
+  profiles.forEach(profile => {
+    profile.classList.remove('selected');
+  });
+
+  // Adicionar a classe "selected" ao perfil clicado
+  profileElement.classList.add('selected');
 }
